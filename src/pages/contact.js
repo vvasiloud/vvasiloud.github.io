1d721a62ff65448265b89edf4caf7dd09feebd88
vvasiloud <5891530+vvasiloud@users.noreply.github.com>
2021-11-02 23:15:56 +0100

    Updated contact form

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,80 +1,30 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { graphql, StaticQuery } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-import axios from 'axios';
+import { useForm, ValidationError } from '@formspree/react';
 
 const ContactForm = ({ formId }) => {
-  const [status, setStatus] = useState({
-    submitted: false,
-    submitting: false,
-    info: { error: false, msg: null }
-  })
-  const [inputs, setInputs] = useState({
-    email: '',
-    message: ''
-  })
-  const handleServerResponse = (ok, msg) => {
-    if (ok) {
-      setStatus({
-        submitted: true,
-        submitting: false,
-        info: { error: false, msg: msg }
-      })
-      setInputs({
-        email: '',
-        message: ''
-      })
-    } else {
-      setStatus({
-        info: { error: true, msg: msg }
-      })
-    }
+  const [state, handleSubmit] = useForm(formId);
+  if (state.succeeded) {
+    return <p>Thank you, your message has been submitted.</p>;
   }
-  const handleOnChange = e => {
-    e.persist()
-    setInputs(prev => ({
-      ...prev,
-      [e.target.id]: e.target.value
-    }))
-    setStatus({
-      submitted: false,
-      submitting: false,
-      info: { error: false, msg: null }
-    })
-  }
-  const handleOnSubmit = e => {
-    e.preventDefault()
-    setStatus(prevStatus => ({ ...prevStatus, submitting: true }))
-    axios({
-      method: 'POST',
-      url: `https://formspree.io/${atob(formId)}`,
-      data: inputs
-    })
-      .then(response => {
-        handleServerResponse(
-          true,
-          'Thank you, your message has been submitted.'
-        )
-      })
-      .catch(error => {
-        handleServerResponse(false, error.response.data.error)
-      })
-  }
-
   return (
     <div>
-      <form onSubmit={handleOnSubmit}>
+      <form onSubmit={handleSubmit}>
         <div className="form-group">
           <label htmlFor="name">Name</label>
           <input
             id="name"
             type="text"
-            name="_name"
-            onChange={handleOnChange}
+            name="name"
             required
-            value={inputs.name}
+          />
+          <ValidationError
+            prefix="Email"
+            field="email"
+            errors={state.errors}
           />
         </div>
         <div className="form-group">
@@ -82,10 +32,13 @@ const ContactForm = ({ formId }) => {
           <input
             id="email"
             type="email"
-            name="_replyto"
-            onChange={handleOnChange}
+            name="email"
             required
-            value={inputs.email}
+          />
+          <ValidationError
+            prefix="Email"
+            field="email"
+            errors={state.errors}
           />
         </div>
         <div className="form-group">
@@ -93,23 +46,18 @@ const ContactForm = ({ formId }) => {
           <textarea
             id="message"
             name="message"
-            onChange={handleOnChange}
             required
-            value={inputs.message}
+          />
+          <ValidationError
+            prefix="Message"
+            field="message"
+            errors={state.errors}
           />
         </div>
-        <button type="submit" disabled={status.submitting}>
-          {!status.submitting
-            ? !status.submitted
-              ? 'Submit'
-              : 'Submitted'
-            : 'Submitting...'}
+        <button type="submit" disabled={state.submitting}>
+          Submit
         </button>
       </form>
-      {status.info.error && (
-        <div className="error">Error: {status.info.msg}</div>
-      )}
-      {!status.info.error && status.info.msg && <p>{status.info.msg}</p>}
     </div>
   )
 };
@@ -130,7 +78,7 @@ const ContactPage = ({ data }, location) => {
       </div>
       <article className="post-content page-template no-image">
         <div className="post-content-body">
-          <ContactForm formId={"dnZhc2lsb3VkaXNAZ21haWwuY29t"}/>
+          <ContactForm formId={"myyelawm"}/>
         </div>
       </article>
     </Layout>
