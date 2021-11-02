d25069b1c820f0e10e05799b1f9fac29dc319dad
vvasiloud <5891530+vvasiloud@users.noreply.github.com>
2021-11-02 22:21:55 +0100

    gh-pages deploy config update

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,6 +2,7 @@ const urljoin = require("url-join")
 const siteConfig = require("./siteConfig")
 
 module.exports = {
+  pathPrefix: "/vvasiloud.github.io",
   siteMetadata: {
     title: siteConfig.name,
     author: siteConfig.author,
